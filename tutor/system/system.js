d68336256bd41df50383262b6d0e06b24d4fe8b1
jim <jimc@bethel.com.tw>
2017-10-04 21:45:10 +0800

    commit

--- tutor/system/system.js
+++ tutor/system/system.js
@@ -69,6 +69,73 @@ system.isSignon = function(){
 	return false;
 }
 system.isTeacher = function(){
-	let i = window.location.href.indexOf("localhost");
-	return i > -1 ? true : false;
+	let i = -1; //window.location.href.indexOf("localhost");
+	if(window.location.href.indexOf("student=") > -1)
+		i = -1;
+	else 
+		i = window.location.href.indexOf("localhost");
+	return i >= 0 ? true : false;
 }
+
+let toast = null;
+window.showToast = function(msg){
+	if(toast != null){
+		$(toast).remove();
+		toast = null;
+	}
+
+	$.toast({
+		heading: 'tutor', // 
+		text: typeof msg == "string" ? msg : msg.msg,
+		icon: typeof msg == "object" && typeof msg.icon == "string" ? msg.icon : "", //'info', 'error', 'success', 'warning'
+		position: typeof msg == "object" && typeof msg.position == "string" ? msg.position : "top-right", //'top-right', 'top-left'
+		hideAfter: typeof msg == "object" && typeof msg.hideAfter == "number" ? msg.hideAfter : 10 * 1000,
+		//stack: false,
+		allowToastClose: typeof msg == "object" && typeof msg.allowToastClose == "boolean" ? msg.allowToastClose : true,
+		afterShown: function(arg){
+			toast = this;
+		},
+		afterHidden: function(){
+			toast = null;
+		}
+	});
+}
+
+var MaskUtil = (function(){ 
+	var $mask, $maskMsg; 
+	var defMsg = '正在處理，請稍待。。。'; 
+	function init(){ 
+		if(!$mask){ 
+			$mask = $("<div class=\"datagrid-mask mymask\"></div>").appendTo("body"); 
+		} 
+		if(!$maskMsg){ 
+			$maskMsg = $("<div class=\"datagrid-mask-msg mymask\">"+defMsg+"</div>") 
+				.appendTo("body").css({'font-size':'16px', height: "auto"}); 
+		} 
+		$mask.css({width:"100%",height: "100%", zIndex: 9990, backgroundColor: "rgb(0,0,0)", opacity: "0.6"}); 
+		var scrollTop = $(document.body).scrollTop(); 
+		$maskMsg.css({ 
+			left: "50%",
+			top: "50%",
+			marginTop: "-60px",
+			marginLeft: "-60px",
+			minWidth: "120px",
+			zIndex: 9991
+		}); 
+	} 
+	return { 
+		mask:function(msg){ 
+			init(); 
+			$mask.show(); 
+			$maskMsg.html(msg||defMsg).show(); 
+		},
+		unmask:function(){ 
+			$mask.remove(); 
+			$maskMsg.remove(); 
+			$mask = undefined;
+			$maskMsg = undefined;
+		}
+	} 
+ }());
+
+
