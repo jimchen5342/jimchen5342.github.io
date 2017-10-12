55ac70684ee501fb182a557566d857fb8f0552d7
jim <jimc@bethel.com.tw>
2017-10-12 20:47:00 +0800

    commit

--- tutor/components/board.js
+++ tutor/components/board.js
@@ -1,15 +1,20 @@
 (function(window, undefined) {
-	let board = {};
+	let board = {}, canvas;
 	board.load = function(base64){
 		//console.log(base64)
+		$("#layoutBoard").html("")
 		var img = new Image();
 		img.onerror = function() {
 		};
 		img.onload = function() {
-
+			canvas = document.createElement('canvas');
+			let ctx = canvas.getContext("2d");
+			canvas.width = $("#layoutBoard").width() - 0;
+			canvas.height = $("#layoutBoard").height() - 0;
+			ctx.drawImage(img,0,0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+			$(canvas).appendTo("#layoutBoard")
 		};
 		img.src = base64;
-		$(img).appendTo("#layoutBoard")
 		$('#winBoard').window('open');
 	}
 	function adjust(){
@@ -38,8 +43,6 @@
 	}
 	setTimeout(function(){
 		adjust();
-		
 	}, 600)
 	window.board = board;
-
 })(window);
