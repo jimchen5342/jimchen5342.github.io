e03ce52ce38c8c9f62e8fbb2b762f88a70f2535d
jim <jimc@bethel.com.tw>
2017-10-26 20:53:35 +0800

    commit

--- tutor/handout/highlight.js
+++ tutor/handout/highlight.js
@@ -9,66 +9,60 @@ $(document).ready(function() {
 });
 
 function loaded(){
-	$('pre code').each(function(i, block) {
-		let arr = block.innerHTML.split("\n");
+	$('pre').each(function(i, block) {
+		let s = "";
+		let arr = block.innerHTML.split("</code>");
+		for(let i = 0; i < arr.length; i++){
+			s += parse(arr[i]);
+		}
+		block.innerHTML = s;
+	});
+
+	function parse(h){
+		let i1 = h.indexOf("<code");
+		let i2 = h.indexOf(">", i1);
+		let start = h.substr(i1, i2 + 1)
+		h = h.substr(i2 + 1);
+		h = h.replace("</code>", "");
+	
+		let arr = h.split("\n");
+		if(arr.length > 0 && arr[0] == "") 
+			arr.splice(0, 1);
+		if(arr.length > 0 && arr[arr.length - 1].trim() == "") 
+			arr.splice(arr.length - 1, 1);
 		let s = "";
 		for(let i = 0; i < arr.length; i++){
 			let row = arr[i];
 			for(let j = 0; j < row.length; j++){
 				let c = row.substr(j, 1);
 				if(c == " " || c == "\t")
-					s += c;
+					s += c == " " ? c : "  ";
 				else
 					break;
 			}
 			if(s.length > 0)
 				break;
 		}
+		console.log("s: " + s.length)
 		for(let i = 0; i < arr.length; i++){
-			arr[i] = arr[i].replace(s, "");
 			if(arr[i].indexOf("\t") > -1)
 				arr[i] = arr[i].replaceAll("\t", "  ");
+			arr[i] = arr[i].replace(s, "");
+			arr[i].trim();
+			console.log(arr[i])
 		}
 		if(arr.length > 0 && arr[0] == "") 
 			arr.splice(0, 1);
 		if(arr.length > 0 && arr[arr.length - 1].trim() == "") 
 			arr.splice(arr.length - 1, 1);
 		//console.log(arr)
-		block.innerHTML = arr.join("\n");
-	});
+		return start.trim() + arr.join("\n") + "</code>";
+	}
 	
 	$('pre code').each(function(i, block) {
 		hljs.highlightBlock(block);
 	});
 	$('code.hljs').each(function(i, block) {
 		hljs.lineNumbersBlock(block);
-	});
-}
-
-function loaded_old(){
-	$('pre code').each(function(i, block) {
-		let arr = block.innerHTML.split("\n");
-		let s = "";
-		for(let i = 0; i < arr.length; i++){
-			let row = arr[i];
-			for(let j = 0; j < row.length; j++){
-				let c = row.substr(j, 1);
-				if(c == " " || c == "\t")
-					s += c;
-				else
-					break;
-			}
-			if(s.length > 0)
-				break;
-		}
-		for(let i = 0; i < arr.length; i++){
-			arr[i] = arr[i].replace(s, "");
-			arr[i] = arr[i].replaceAll("\t", "  ");
-		}
-		block.innerHTML = arr.join("\n");
-	});
-	
-	$('pre code').each(function(i, block) {
-		hljs.highlightBlock(block);
 	});
 }
