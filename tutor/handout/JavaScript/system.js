c8571362dfc1ae88b24cdd8b8144b3a17d8f55ff
jim <jimc@bethel.com.tw>
2017-12-24 10:49:05 +0800

    commit

--- tutor/handout/JavaScript/system.js
+++ tutor/handout/JavaScript/system.js
@@ -1,4 +1,7 @@
-function write(s) {
+function write(s, v) {
 	let body = document.getElementsByTagName('body')[0];
-	body.innerHTML += s + "<br/>";
+
+	body.innerHTML += 
+		(arguments.length == 2 ? "<span style='color: red'>" + s + ": </span>" + v : s)
+		 + "<br/>";
 }
