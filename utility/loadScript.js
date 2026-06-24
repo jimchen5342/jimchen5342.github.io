5c7cefbebdd6e9c146b80078fe3d719f1557375c
jim <jimc@bethel.com.tw>
2026-06-24 17:05:15 +0800

    2026-06-24

--- utility/loadScript.js
+++ utility/loadScript.js
@@ -1,45 +1,57 @@
-let loadScript = function(filename, callback ){
-	let arrScript = [];
-	if( typeof filename == "string" )
-		arrScript.push( filename );
-	else
-		arrScript = filename;
+let loadScript = function(filename){
+	return new Promise(function(resolve, reject){
+		let arrScript = [];
+		if( typeof filename == "string" )
+			arrScript.push( filename );
+		else
+			arrScript = filename;
 
-	function start( index ){
-		if( index == arrScript.length ){
-			if( callback ) callback();
-			return;
-		} else if (arrScript[index].indexOf(".css") > 1){ //if filename is an external CSS file
-			let link=document.createElement("link");
-			link.setAttribute("rel", "stylesheet");
-			link.setAttribute("type", "text/css");
-			link.setAttribute("href", arrScript[index]);
-			link.className    = "css";
-			if(typeof link!="undefined")
-				document.getElementsByTagName("head")[0].appendChild(fileref)
-			start(index+1);
-		} else if (arrScript[index].indexOf(".js") > 1){
-			let script   = document.createElement("script");
-			script.type  = "text/javascript";
-			script.src   = arrScript[index]; //filename;
-			script.className    = "js";
-			if(typeof script != "undefined"){
+		function start( index ){
+			if( index === arrScript.length ){
+				resolve();
+				return;
+			}
+
+			const item = arrScript[index];
+			if(!item){
+				start(index+1);
+				return;
+			}
+
+			if (item.indexOf(".css") > -1){ // external CSS file
+				let link = document.createElement("link");
+				link.setAttribute("rel", "stylesheet");
+				link.setAttribute("type", "text/css");
+				link.setAttribute("href", item);
+				link.className = "css";
+				document.getElementsByTagName("head")[0].appendChild(link);
+				start(index+1);
+			} else if (item.indexOf(".js") > -1){
+				let script = document.createElement("script");
+				script.type = "text/javascript";
+				script.src = item;
+				script.className = "js";
 				script.onload = function(){
 					start(index+1);
 				};
+				script.onerror = function(){
+					reject(new Error('Failed to load script: ' + item));
+				};
 				document.getElementsByTagName("head")[0].appendChild(script);
-			} else
+			} else {
 				start(index+1);
-		} else
-			start(index+1);
-	}
-	removeClass(".js");
-	removeClass(".css");
-	start(0);
-	function removeClass(cls) {
-		let els = document.querySelectorAll(cls)
-		for(let i = 0; i < els.length; i++) {
-			els[i].parentNode.removeChild(els[i]);
+			}
 		}
-	}
+
+		removeClass(".js");
+		removeClass(".css");
+		start(0);
+
+		function removeClass(cls) {
+			let els = document.querySelectorAll(cls)
+			for(let i = 0; i < els.length; i++) {
+				els[i].parentNode.removeChild(els[i]);
+			}
+		}
+	});
 };
---
+++ utility/setting.js
@@ -0,0 +1,3 @@
+export const setting = {
+   lessons: ['第 01 課', '第 02 課', '第 03 課']
+};
